69a702a032463aae811fe7477155a53292aed83a
Tom Henricksen <tom.henricksen@gmail.com>
2019-04-23 20:21:57 -0400

    Create Floor.java

---
+++ com/myitcareercoach/javamasterclass/Floor.java
@@ -0,0 +1,20 @@
+public class Floor {
+
+	private double width;
+	
+	private double length;
+
+
+	public Floor(double width, double length) {
+		super();
+		if(width < 0) {
+			width = 0;
+		}
+		if(length < 0) {
+			length = 0;
+		}
+		this.width = width;
+		this.length = length;
+	}
+	
+}
